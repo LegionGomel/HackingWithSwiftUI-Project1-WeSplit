7aa344648d6a041d203b15525b893094716fab16
Yuri Ershov <shtorm0606@gmail.com>
2022-01-11 13:23:00 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Yuri Ershov on 11.01.22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
